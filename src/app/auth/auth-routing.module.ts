6f6299919061e42c43e4e3c9a5ec53f2a2858e02
= <=>
2023-07-07 08:12:49 -0600

    default path in auth routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
       {
         path: 'olvide-password',
         component: ForgotPasswordComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'login',
       }
     ]
   }
