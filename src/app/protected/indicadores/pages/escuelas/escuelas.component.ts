674c248c6973997131de11aa7ec8fc4d176e3d2d
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-07-06 22:06:57 -0600

    show school indicador information

--- src/app/protected/indicadores/pages/escuelas/escuelas.component.ts
+++ src/app/protected/indicadores/pages/escuelas/escuelas.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EscuelaService } from '../../services/escuelas.service';
+import { School } from '../../interfaces/schools';
 
 @Component({
   selector: 'app-escuelas',
   templateUrl: './escuelas.component.html'
 })
-export class EscuelasComponent {
+export class EscuelasComponent implements OnInit {
+
+  indicadores :School[] = [];
+
+  constructor(private schoolService: EscuelaService) {}
+
+  ngOnInit(): void {
+    this.schoolService.getIndicadoresSchool().subscribe({
+      next: resp => {
+        console.log(resp);
+        this.indicadores = resp.contenido;
+      },
+      error: err => console.log(err),
+    });
+  }
 
 }
--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'begin',
+    path: '',
     loadChildren: () => import('./indicadores/indicadores.module').then(m => m.IndicadoresModule),
   },
 ];
