b72b39e9f8c4a90da9f2962c7035edf092dd0b82
= <=>
2023-07-07 08:17:56 -0600

    add reactive form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+  authForm = this._fb.group({
+    user: ['', [Validators.required] ],
+    password: ['', [Validators.required] ],
+  });
+
+  constructor(private _fb: FormBuilder, private _router: Router) {}
+
+  onSubmit() {
+    this._router.navigateByUrl('/begin/schools');
+  }
 
 }
